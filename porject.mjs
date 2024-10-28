14ae499f781ddc16f7f1de0c2e5fe6a3b4714275
IgRiZ1 <dalil.blh@outlook.be>
2024-10-28 10:56:42 +0100

    Create porject.mjs

---
+++ porject.mjs
@@ -0,0 +1,73 @@
+
+import readline from 'node:readline';
+import { stdin as input, stdout as output } from 'node:process';
+const rl = readline.createInterface({ input, output });
+
+let prijs = { W: 10, B: 2, F: 3 };
+let btwprijs = { W: 21, B: 6, F: 12 };
+
+let mandgroot = 0;
+let mand = [];
+
+
+function vraagGrootteMand() {
+  rl.question("Hoe lang is de mand (3 - 20)? ", (antwoord) => {
+    mandgroot = parseInt(antwoord, 10);
+
+    if (mandgroot >= 3 && mandgroot <= 20) {
+      vulGeschenkmand(0); 
+    } else {
+      console.log("Ongeldige . opnieuw en sneller.");
+      vraagGrootteMand();
+    }
+  });
+}
+
+
+function vulGeschenkmand(i) {
+  if (i >= mandgroot) {
+    return berekenTotaal(); 
+  }
+
+  rl.question("Welk CADEAU kies je? (keuze: W, B, F): ", (keuze) => {
+    if (keuze === "W" || keuze === "B" || keuze === "F") {
+      mand.push(keuze);
+      i++;
+
+      
+      if (Math.random() < 0.1) {
+        console.log("JE HEBT EEN CADEAU GEWONNEN!");
+        mand.push(keuze); 
+        i++;
+      }
+
+      vulGeschenkmand(i); 
+    } else {
+      console.log("slecht mevrouwtje of meneer, probeer opnieuw.");
+      vulGeschenkmand(i); 
+    }
+  });
+}
+
+function berekenTotaal() {
+  let totaal = 0;
+  for (let i = 0; i < mand.length; i++) {
+    totaal += prijs[mand[i]];
+  }
+  console.log(`De totaal van je mand is: €${totaal}`);
+
+
+  let totaalMetBTW = 0;
+  for (let i = 0; i < mand.length; i++) {
+    let item = mand[i];
+    let btw = prijs[item] * (btwprijs[item] / 100);
+    totaalMetBTW += prijs[item] + btw;
+  }
+  console.log(`de BTW is dit: €${totaalMetBTW.toFixed(2)}`);
+
+  rl.close();
+}
+
+
+vraagGrootteMand();
+
