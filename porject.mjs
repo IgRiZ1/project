e3b57226546934b49b4a4da7c17733c9fdb8f407
IgRiZ1 <dalil.blh@outlook.be>
2024-11-03 11:42:36 +0100

    Update porject.mjs

--- porject.mjs
+++ porject.mjs
@@ -1,7 +1,7 @@
 
 import readline from 'node:readline';
 import { stdin as input, stdout as output } from 'node:process';
-const rl = readline.createInterface({ input, output });
+const dalil = readline.createInterface({ input, output });
 
 let prijs = { W: 10, B: 2, F: 3 };
 let btwprijs = { W: 21, B: 6, F: 12 };
@@ -11,8 +11,8 @@ let mand = [];
 
 
 function vraagGrootteMand() {
-  rl.question("Hoe lang is de mand (3 - 20)? ", (antwoord) => {
-    mandgroot = parseInt(antwoord, 10);
+  dalil.question("Hoe lang is de mand (3 - 20)? ", antwoord => {
+    mandgroot = +antwoord;
 
     if (mandgroot >= 3 && mandgroot <= 20) {
       vulGeschenkmand(0); 
@@ -29,7 +29,7 @@ function vulGeschenkmand(i) {
     return berekenTotaal(); 
   }
 
-  rl.question("Welk CADEAU kies je? (keuze: W, B, F): ", (keuze) => {
+  dalil.question("Welk CADEAU kies je? (keuze: W, B, F): ", (keuze) =>{
     if (keuze === "W" || keuze === "B" || keuze === "F") {
       mand.push(keuze);
       i++;
@@ -52,7 +52,7 @@ function vulGeschenkmand(i) {
 function berekenTotaal() {
   let totaal = 0;
   for (let i = 0; i < mand.length; i++) {
-    totaal += prijs[mand[i]];
+    totaal += prijs[mand[i]]; 
   }
   console.log(`De totaal van je mand is: €${totaal}`);
 
@@ -61,11 +61,11 @@ function berekenTotaal() {
   for (let i = 0; i < mand.length; i++) {
     let item = mand[i];
     let btw = prijs[item] * (btwprijs[item] / 100);
-    totaalMetBTW += prijs[item] + btw;
+    totaalMetBTW += prijs[item] + btw; 
   }
-  console.log(`de BTW is dit: €${totaalMetBTW.toFixed(2)}`);
+  console.log("de BTW is dit: €" + Math.round(totaalMetBTW));
 
-  rl.close();
+  dalil.close();
 }
 
 
